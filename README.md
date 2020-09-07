caae527eff7617c0012404e036b9163eb9f13aaf
Frédéric BISSON <zigazou@free.fr>
2020-09-07 08:35:36 +0200

    Added social preview image Updated README

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ MyTerminal
 
 MyTerminal is a serial terminal implemented on an FPGA.
 
+![MyTerminal](doc/myterminal.png)
+
 Characteristics
 ---------------
 
@@ -12,6 +14,9 @@ Characteristics
   - 16×20 1024 character set
   - semi-graphic characters
 - fast serial input (tested at 1Mbps)
+  - CTSRTS control signal when doing “intensive” operations
+  - 8 characters FIFO 
+  - UTF-8 support
 - low cost (cheap FPGA, 9 resistor DAC…)
 - written in Verilog
 - inspired by Videotex and text mode video card
---
+++ demo/diagonal.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import random
+
+random.choice(['a', 'b', 'c'])
+
+diagonals = [
+    0xE211,
+    0xE212,
+    0xE213,
+    0xE214,
+    0xE215,
+    0xE216,
+    0xE217,
+    0xE218,
+    0xE219,
+    0xE21A,
+    0xE21B,
+    0xE21C,
+    0xE21F,
+    0xE221,
+    0xE222,
+    0xE223,
+    0xE224,
+    0xE225,
+    0xE226,
+    0xE227,
+    0xE228,
+    0xE229,
+    0xE22A,
+    0xE22B,
+    0xE22C,
+    0xE22D,
+    0xE22E,
+    0xE22F
+]
+
+screen = ""
+for _ in range(80*51):
+    screen += chr(random.choice(diagonals))
+
+print(chr(1) + screen, end='')
